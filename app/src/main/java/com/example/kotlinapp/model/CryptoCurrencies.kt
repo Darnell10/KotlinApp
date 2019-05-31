bfedfbac0ad6dad613917099473082d28e999f51
Darnell O <DarnellOtto@ac.c4q.nyc>
2019-05-30 20:29:35 -0400

    Removed info

--- app/src/main/java/com/example/kotlinapp/model/CryptoCurrencies.kt
+++ app/src/main/java/com/example/kotlinapp/model/CryptoCurrencies.kt
@@ -3,3 +3,5 @@ package com.example.kotlinapp.model
  data class CryptoCurrencies (val currency : String , val price : String)
 
 
+
+
--- app/src/main/java/com/example/kotlinapp/networking/ApiInterface.kt
+++ app/src/main/java/com/example/kotlinapp/networking/ApiInterface.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinapp.networking
 
+import com.example.kotlinapp.BuildConfig
 import com.example.kotlinapp.model.CryptoCurrencies
+import com.example.kotlinapp.model.Interval
 import io.reactivex.Observable
 import retrofit2.http.GET
 
@@ -8,8 +10,15 @@ interface ApiInterface {
 
     //Describe the request type and relative Url
 
-    @GET("prices?key=2e323319505fee9d82a1c85d3f49e765")
+    val apiKey : String
+        get() = BuildConfig.ApiKey
+
+    @GET("prices?key=" )
     fun getData() : Observable<List<CryptoCurrencies>>
+
+    @GET("currencies/interval?key= ")
+    fun getMoreData():Observable<List<Interval>>
+
 
 
 }
